f39c1e2c7e3bed27406587c0a76dac2ff2841d0f
ronco101 <roncohen408@gmail.com>
2024-02-14 15:05:17 -0500

    Gir removed all my test cases
    I fucking hate github desktop holy shit. why does it hate me

---
+++ memtest.h
@@ -0,0 +1,9 @@
+#ifndef TEST_H
+#define TEST_H
+
+void test_check();
+void *test_myfree_valid_pointer();
+void *test_myfree_null_pointer();
+void *test_myfree_int_free();
+
+#endif
--- test.h
+++
@@ -1,8 +0,0 @@
-#ifdef TEST_H
-#define TEST_H
-
-void test_myfree_valid_pointer();
-void test_myfree_null_pointer();
-void test_myfree_int_free();
-
-#endif
